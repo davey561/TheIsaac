d57dce19cbd86eab82068d3d46606629744f2d9a
Davey Morse <davey561@gmail.com>
2019-07-31 15:22:58 -0400

    first commit baby--plexus p

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import Todos from './Todos';
+import CytoscapeComponent from 'react-cytoscapejs';
 
 function App() {
+  const [state, setState] = useState({
+    todos: [
+      {id: 1, content: 'buy some milk'},
+      {id: 2, content: 'Isaac julia sex'}
+    ]
+  });
+  // const [elements, setElements] = useState(
+  //     { data: { id: 'one', label: 'Node 1' }, position: { x: 0, y: 0 } },
+  //     { data: { id: 'two', label: 'Node 2' }, position: { x: 100, y: 0 } },
+  //     { data: { source: 'one', target: 'two', label: 'Edge from Node1 to Node2' } }
+  // );
+  const deleteTodo = (id) => {
+    const todos = state.todos.filter(todo => {
+      return todo.id != id
+    })
+    setState({
+      todos: todos
+    })
+  }
+  const elements = [
+    { data: { id: 'one', label: 'Node 1' }, position: { x: 0, y: 0 } },
+    { data: { id: 'two', label: 'Node 2' }, position: { x: 100, y: 0 } },
+    { data: { source: 'one', target: 'two', label: 'Edge from Node1 to Node2' } }
+  ];
+  const layout = { name: 'cose' };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="todo-app container">
+      <h1 className="">Plexus P</h1>
+      <CytoscapeComponent elements={elements} style={ { width: '600px', height: '600px' }} layout={layout} />
     </div>
   );
 }
