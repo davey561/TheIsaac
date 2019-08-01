a96544a8295d0dc35ab51c3bda2fc16be5803494
Davey Morse <davey561@gmail.com>
2019-08-01 12:31:33 -0400

    changed to class components, event responses work

--- src/App.js
+++ src/App.js
@@ -2,27 +2,24 @@ import React, {useState} from 'react';
 import Todos from './Todos';
 import Plexus from './Plexus'
 
-function App() {
-  const [state, setState] = useState({
-    todos: [
-      {id: 1, content: 'buy some milk'},
-      {id: 2, content: 'Isaac julia sex'}
-    ]
-  });
-  const todos = (id) => state.todos.filter(todo => {
-    return todo.id != id
-  })
-  // const [elements, setElements] = useState(
-  //     { data: { id: 'one', label: 'Node 1' }, position: { x: 0, y: 0 } },
-  //     { data: { id: 'two', label: 'Node 2' }, position: { x: 100, y: 0 } },
-  //     { data: { source: 'one', target: 'two', label: 'Edge from Node1 to Node2' } }
-  // );
-  return (
-    <div className="todo-app container">
-      <h1 className="">Plexus P</h1>
-      <Plexus />
-    </div>
-  );
+class App extends React.Component {
+  constructor(props){
+    super(props)
+  }
+  componentDidMount(){
+    // this.refs.graph.cy.on('click', function(){
+    //   window.alert('brushed my teeth with a bottle of jack');
+    // })
+  }
+  render(){
+    //ref="graph"
+    return (
+      <div className="todo-app container">
+        <h1 className="">Plexus P</h1>
+        <Plexus />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/Plexus.js
+++ src/Plexus.js
@@ -2,32 +2,62 @@
 import React, {useState, useEffect} from 'react';
 import CytoscapeComponent from 'react-cytoscapejs';
 
-function Plexus(props){
-    // const [cy, setCy] = useState({
-    // })
-    let cyRef = React.createRef();
-    const elements = [
-    { data: { id: 'one', label: 'Node 1' }, position: { x: 0, y: 0 } },
-    { data: { id: 'two', label: 'Node 2' }, position: { x: 100, y: 0 } },
-    { data: { source: 'one', target: 'two', label: 'Edge from Node1 to Node2' } }
-    ];
-    const layout = { name: 'cose' };
-    const testfx = () => {
-        window.alert(cy.nodes()[0].style('label'));
+class Plexus extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            elements: [
+                { data: { id: 'one', label: 'Node 1' }, position: { x: 0, y: 0 } },
+                { data: { id: 'two', label: 'Node 2' }, position: { x: 100, y: 0 } },
+                { data: { source: 'one', target: 'two', label: 'Edge from Node1 to Node2' } }
+            ],
+            layout: { name: 'cose' },
+            cy: {}
+        }
     }
-    useEffect(() => {
-        
-    });
-    return (
+    componentDidMount(){
+        this.cy.on('click', function(){
+            window.alert('shake it');
+        })
+    }
+    getCy(){
+        return this.cy;
+    }
+    render(){
+        return (
         <div>
             <CytoscapeComponent 
-                elements={elements} 
+                elements={this.state.elements} 
                 style={ { width: '100%', height: '620px' }} 
-                layout={layout} 
-                cy={(cy) => { myCyRef = cy }}
+                layout={this.state.layout} 
+                cy={(cy) => { this.cy = cy }}
             />
-            <button onClick={testfx}>test</button>
+            {/* <button onClick={testfx}>test</button> */}
         </div>
-    )
+        );
+    }
 }
+// function Plexus (){
+//     // const[cy, setCy] = useState(null);
+//     const[eles, setEles] = useState(
+//         { data: { id: 'one', label: 'Node 1' }, position: { x: 0, y: 0 } },
+//         { data: { id: 'two', label: 'Node 2' }, position: { x: 100, y: 0 } },
+//         { data: { source: 'one', target: 'two', label: 'Edge from Node1 to Node2' } }
+//     )
+//     const [layout, setLayout] = useState({ name: 'cose' });
+//     useEffect(() => this.refs.cy.on('click', function(){
+//         window.alert('shake it');
+//     }));
+//     return (
+//         <div>
+//             <CytoscapeComponent 
+//                 elements={eles} 
+//                 style={ { width: '100%', height: '620px' }} 
+//                 layout={layout} 
+//                 cy={(cy) => { this.cy = cy }}
+//             />
+//             {/* <button onClick={testfx}>test</button> */}
+//         </div>
+//     );
+// }
 export default Plexus;
